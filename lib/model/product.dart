f279e3cb29ad6f0633fb6f819cb23fadd738e228
ganesh <syntax.bugfix@gmail.com>
2024-02-13 14:44:15 +0530

    Initial commit

---
+++ lib/model/product.dart
@@ -0,0 +1,57 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'product.g.dart';
+
+@JsonSerializable()
+class Product {
+  @JsonKey(name: "id")
+  String? id;
+
+  @JsonKey(name: "name")
+  String? name;
+
+  @JsonKey(name: "description")
+  String? description;
+
+  @JsonKey(name: "category")
+  String? category;
+
+  @JsonKey(name: "image")
+  String? image;
+
+  @JsonKey(name: "price")
+  double? price;
+
+  @JsonKey(name: "brand")
+  String? brand;
+
+  @JsonKey(name: "offer")
+  bool? offer;
+
+  Product({
+    this.id,
+    this.name,
+    this.description,
+    this.category,
+    this.image,
+    this.offer,
+    this.price,
+    this.brand,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      category: json['category'],
+      image: json['image'],
+      // Handle non-numeric 'price' value
+      price: json['price'] is num ? json['price'].toDouble() : 0.0,
+      brand: json['brand'],
+      offer: json['offer'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => _$ProductToJson(this);
+}
